9319d833b29598e38e6c0e58f0dd1864ac385c1c
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-16 11:07:27 -0300

    Add files Home routes

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,6 +4,10 @@ import React from 'react';
 
 import { useState, useEffect } from 'react';
 
+import "./Home.css";
+
+import { Link } from "react-router-dom";
+
 const Home = () => {
  
 const [posts, setposts] = useState([])
@@ -12,7 +16,10 @@ const getPosts = async() => {
   try {
     const response = await axios.get("https://jsonplaceholder.typicode.com/posts"
     );
-    console.log(response);
+
+    const data = response.data;
+
+    setposts(data);
   } catch (error) {
     console.log(error);
   }
@@ -20,8 +27,27 @@ const getPosts = async() => {
 
 useEffect(() => {
   getPosts()
-}, [])
-  return <div>Home</div>
-}
+}, []);
 
-export default Home
+
+  return (
+  <div>
+    <h1>Últimos Posts</h1>
+    {posts.length === 0 ? (
+    <p>Carregando...</p>
+    ) : (
+      posts.map((post) => (
+        <div className='post' key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+          <Link to={`/posts/${post.id}`} className="btn">
+            Ler mais
+          </Link>
+        </div>
+      ))
+    )}
+  </div>
+  );
+};
+
+export default Home;
