7363f874e2e316801a32e8eece4846f2328c65ba
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-16 10:40:31 -0300

    Add routes and axios configurations

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import axios from 'axios';
+
+import React from 'react';
+
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+ 
+const [posts, setposts] = useState([])
+
+const getPosts = async() => {
+  try {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts"
+    );
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}; 
+
+useEffect(() => {
+  getPosts()
+}, [])
   return <div>Home</div>
 }
 
