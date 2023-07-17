26ae29e7c1d7ddfb2fe15fac3c5b602fb6fa2f89
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-17 12:33:35 -0300

    Add files Axios config

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,52 +1,47 @@
-import axios from 'axios';
+import blogFetch from "../axios/config.js";
 
-import React from 'react';
-
-import { useState, useEffect } from 'react';
-
-import "./Home.css";
+import { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
+import "./Home.css";
+
 const Home = () => {
- 
-const [posts, setposts] = useState([])
+  const [posts, setPosts] = useState([]);
 
-const getPosts = async() => {
-  try {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/posts"
-    );
+  const getPosts = async () => {
+    try {
+      const response = await blogFetch.get("/posts");
 
-    const data = response.data;
+      const data = response.data;
 
-    setposts(data);
-  } catch (error) {
-    console.log(error);
-  }
-}; 
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-useEffect(() => {
-  getPosts()
-}, []);
-
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
-  <div>
-    <h1>Últimos Posts</h1>
-    {posts.length === 0 ? (
-    <p>Carregando...</p>
-    ) : (
-      posts.map((post) => (
-        <div className='post' key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-          <Link to={`/posts/${post.id}`} className="btn">
-            Ler mais
-          </Link>
-        </div>
-      ))
-    )}
-  </div>
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link className="btn" to={`/posts/${post.id}`}>
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
   );
 };
 
